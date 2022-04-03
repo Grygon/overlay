33060d711e9d3b2913b8777945ea4ff7edabc25d
Jakob Rubin <647846+Grygon@users.noreply.github.com>
2022-04-02 20:53:37 -0500

    Add sample data

--- userscript.user.js
+++ userscript.user.js
@@ -24,8 +24,8 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const j = document.createElement("img");
-		j.src = "https://github.com/Atkion/placeCosmereOverlay/raw/master/template.png";
-		j.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+                j.src = "https://github.com/Atkion/placeCosmereOverlay/raw/master/template.png";
+                j.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
                 console.log(j);
                 return j;
             })())
